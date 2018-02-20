25ab19a042add8e569bfcdb296cd852559cc7276
pavolbincik <pavolbinck@gmail.com>
2018-02-20 19:38:39 +0100

    change hostname

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,8 +4,8 @@ let config = {
     host: 'https://chess-analysis.com'
 };
 if (isDev) {
-    config.host = 'localhost';
+    config.host = 'localhost:8080';
 }
-config.host = 'localhost:8080';
+
 
 export default config;
