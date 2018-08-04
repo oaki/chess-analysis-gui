c12eb0be63f35771556151fba695593e35d32ec1
pavolbincik <pavolbinck@gmail.com>
2018-08-04 18:11:31 +0200

    change hostname for window.opener.postMessage(jwtValues, window.location.origin);

--- src/layouts/auth/googlePopupRedirect.tsx
+++ src/layouts/auth/googlePopupRedirect.tsx
@@ -5,7 +5,7 @@ import config from "../../config/index";
 
 export default function GooglePopupRedirect() {
 
-    const redirectUri = "http://localhost:3000/auth/verify-google-response";
+    const redirectUri = `${window.location.origin}/auth/verify-google-response`;
     const scope = "https://www.googleapis.com/auth/userinfo.email";
     const responseType = "id_token";
     const href = `https://accounts.google.com/o/oauth2/auth?redirect_uri=${redirectUri}&client_id=${config.google.clientId}&scope=${scope}&response_type=${responseType}`
